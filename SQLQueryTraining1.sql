4d6f22203167394a48cae0c5ace888173fb4e45f
зゝ∠ <Pantynopants@users.noreply.github.com>
2022-06-27 13:04:53 -0500

    Update SQLQueryTraining1.sql

--- SQLQueryTraining1.sql
+++ SQLQueryTraining1.sql
@@ -66,3 +66,28 @@ ORDER BY Name
 --12. retrieves the unique combination of columns ProductSubcategoryID and Color from the Production.Product table. We do not want any rows that are NULL.in any of the two columns in the result. (Hint: Use IsNull)
 SELECT distinct ISNULL(p.ProductSubcategoryID, '0'), ISNULL(p.Color, 'No Color')
 FROM Production.Product p
+
+
+
+
+-------------------------------------------------------------------------------------------------------------
+
+
+--please check the solution below:
+USE [AdventureWorks2019] 
+----- 1 ---- select productid,Name,Color,ListPrice from [Production].[Product]
+----- 2 ----- select productid,Name,Color,ListPrice from [Production].[Product] where ListPrice 0
+----- 3 -----select productid,Name,Color,ListPrice from [Production].[Product]  where color is null
+----- 4 ----- select productid,Name,Color,ListPrice from [Production].[Product] where color is not null and ListPrice > 0
+------ 5 ---- select Name+':'+color from production.Product where color is not null
+----- 6 ----- select 'NAME: '+ name+' -- COLOR: '+ color from production.product where color is not null
+------ 7 ------- select productid,Name from [Production].[Product] where productid between 400 and 500
+----- 8 ------ select productid,Name,Color from [Production].[Product] where color in ('Black','Blue')
+------ 9 ----- select productid,Name,Color,ListPrice from [Production].[Product] where name like 'S%' 
+
+------ 10 ------ select Name,ListPrice from Production.Product where Name like 'S%' or Name like 'A%' 
+----- 11 ------ SELECT [Name], ListPrice  FROM Production.Product WHERE [Name] LIKE 'spo[^k]%' ORDER BY [Name]
+
+------ 12 ------ select distinct ProductSubcategoryID ,Color from Production.Product where Color is not null and ProductSubcategoryID is not null order by ProductSubcategoryID,Color
+
+
